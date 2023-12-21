a719aa06b3219fee7454575cb7217cf0f21bd76e
Nicolas Arnold <narnold@cl.uni-heidelberg.de>
2023-12-22 00:32:59 +0100

    Add missing gem for current ghpages jekyll version

--- config.rb
+++ config.rb
@@ -1,13 +1,16 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
+#
+gems: 
+- webrick
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "_site/assets/css/"
-css_dir = "assets/css/compiled/"
-sass_dir = "assets/css/"
-images_dir = "assets/img"
-javascripts_dir = "assets/js"
+http_path = '/'
+css_dir = '_site/assets/css/'
+css_dir = 'assets/css/compiled/'
+sass_dir = 'assets/css/'
+images_dir = 'assets/img'
+javascripts_dir = 'assets/js'
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -17,7 +20,6 @@ relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
